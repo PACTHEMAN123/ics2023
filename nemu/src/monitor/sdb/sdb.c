5433350991caab435b5c91ee0ae8f84d08914e8d
tracer-ics2023 <tracer@njuics.org>
2024-03-22 21:53:14 +0800

    >  compile NEMU 2023311422 pacman Linux matebook 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:53:14 up 3 days, 23:44,  3 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -94,11 +94,14 @@ static int cmd_x(char *args) {
   else {
     nums = atoi(num);	
     exprs = strtol(nexpr, NULL, 16);
+    printf("%d, %x",nums, exprs);
+    /*
     int i;
     for(i = 0; i < nums; i++) {
       word_t vread = vaddr_read(exprs+4*i, 4);
       printf("%x\n",vread);  
     }
+    */
   }
   return 0;
 }
