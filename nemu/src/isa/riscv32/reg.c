29cce629d99738cc56ae7c10480c41e586ff2d3a
tracer-ics2023 <tracer@njuics.org>
2024-03-22 16:10:40 +0800

    >  compile NEMU 2023311422 pacman Linux matebook 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:10:40 up 3 days, 18:02,  3 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -23,7 +23,12 @@ const char *regs[] = {
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
-void isa_reg_display() {
+void isa_reg_display(void) {
+  int i;
+  for(i = 0; i < 32; i++) {
+    printf("%s %x %d\n", regs[i], cpu.gpr[i], cpu.gpr[i]); 
+  }
+  return; 
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
