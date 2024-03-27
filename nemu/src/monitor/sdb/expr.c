425a84ab4b566d99cc15bc90f333905246f113a6
tracer-ics2023 <tracer@njuics.org>
2024-03-27 21:06:27 +0800

    >  compile NEMU 2023311422 pacman Linux matebook 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:06:27 up 8 days, 22:56,  3 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -75,6 +75,75 @@ typedef struct token {
 static Token tokens[32] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
+int findop(int p, int q) {
+  int i,op=0;
+  int islock = 0;
+  for(i = p; i <= q; i++) {
+    if(tokens[i].type == TK_NUM){continue;}
+    if(tokens[i].type == '('){islock = 1;continue;}
+    if(tokens[i].type == ')'){islock = 0;continue;}
+    if(!islock) {
+    	if(tokens[i].type == '+'||tokens[i].type == '-'){return i;}
+	else {
+	  if(!islock) { op = i; }
+	}
+    }
+  }
+  return op;
+}
+
+static bool check_parentheses(int p, int q) {
+  if(tokens[p].type!='('||tokens[q].type!=')'){ return false; }
+  
+  char par[32];
+  int i,nr_p = 0;
+  for(i = p; i<=q; i++){
+    if(tokens[i].type=='('||tokens[i].type==')') {
+      par[nr_p] = tokens[i].type;
+      nr_p ++;
+    }
+  } 
+
+  if(p+1 > q-1)return false;
+
+  int stack = 0;
+  for(i = 1; i < nr_p-1; i++) {
+    if(par[i]=='('){ stack++;}
+    else{stack--;}
+    if(stack < 0){return false;}
+  }
+
+  if(stack){ return false;}
+  return true;
+}
+
+uint32_t eval(int p, int q){
+  if(p > q) {
+    return -1; 
+  }
+  else if(p == q) {
+    if(tokens[p].type != TK_NUM)return -1;
+    else return ((uint32_t)atoi(tokens[p].str));
+  }
+  else if(check_parentheses(p, q) == true) {
+    return eval(p + 1, q - 1);
+  }
+  else {
+    int op = findop(p, q);
+    uint32_t val1 = eval(p, op - 1);
+    uint32_t val2 = eval(op + 1, q);
+    switch(tokens[op].type) {
+      case '+': return val1 + val2;
+      case '-': return val1 - val2;
+      case '*': return val1 * val2;
+      case '/': 
+		assert(val2);
+		return val1 / val2;
+      default: assert(0);
+    }
+  }
+}
+
 static bool make_token(char *e) {
   int position = 0;
   int i;
@@ -103,6 +172,7 @@ static bool make_token(char *e) {
            case TK_NOTYPE: break;
 	   case TK_NUM: 
 		int j;
+		assert(substr_len >= 32);
 	        for(j = 0; j < substr_len; j++){ tokens[nr_token].str[j] = substr_start[j];}	
 	   default: 
 		tokens[nr_token].type = rules[i].token_type;
