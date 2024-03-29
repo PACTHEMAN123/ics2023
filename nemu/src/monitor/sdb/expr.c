b98bdea407627a6bda4fce84ad2c038b10f93333
tracer-ics2023 <tracer@njuics.org>
2024-03-29 17:04:36 +0800

    >  compile NEMU 2023311422 pacman Linux matebook 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:04:36 up 10 days, 18:53,  4 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -133,7 +133,7 @@ uint32_t eval(int p, int q){
     uint32_t val1 = eval(p, op - 1);
     uint32_t val2 = eval(op + 1, q);
     
-    printf("%d%c%d\n",val1,tokens[op].type,val2);
+    //printf("%d%c%d\n",val1,tokens[op].type,val2);
     
     switch(tokens[op].type) {
       case '+': return val1 + val2;
