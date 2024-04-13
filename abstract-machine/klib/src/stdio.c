7c5b592e17c411a5cbc279be1bbe64ba2543e2c9
tracer-ics2023 <tracer@njuics.org>
2024-04-13 16:33:36 +0800

    >  compile NEMU 2023311422 pacman Linux matebook 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:33:36 up  7:08,  5 users,  load average: 0.00, 0.02, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,44 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  int d;
+  char *s;
+  char str[12];  
+  
+  va_start(ap, fmt);
+  while (*fmt) {
+    if(*fmt == '%') {
+      fmt ++;
+      if(*fmt == 'd') {
+	// demical number operation
+	d = va_arg(ap, int);
+	// int to string
+	int i = 0;
+	while (d != 0) {
+	  str[i++] = (d % 10) + '0';
+	  d /= 10;
+	}
+	while (i > 0) { *out++ = str[--i]; }
+      } 
+      else if(*fmt == 's') {
+	// string operation
+	s = va_arg(ap, char *);
+	while(*s) {
+	  *out++ = *s++ ;	
+	}
+      }
+      else {
+	fmt --; 
+      }
+    }
+    else {
+      *out++ = *fmt++ ;
+    }
+  }
+  *out = '\0';
+  va_end(ap);
+  return strlen(out); 
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
