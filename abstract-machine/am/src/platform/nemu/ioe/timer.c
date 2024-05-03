3216bbde1eeae5647c411e3804baeec64068c995
tracer-ics2023 <tracer@njuics.org>
2024-05-03 09:45:41 +0800

    >  compile NEMU 2023311422 pacman Linux matebook 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:45:41 up 11 days, 38 min,  3 users,  load average: 0.00, 0.00, 0.00

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,11 +1,15 @@
 #include <am.h>
 #include <nemu.h>
 
+uint64_t boot_time = 0;
+
 void __am_timer_init() {
+  boot_time = inl(RTC_ADDR); 
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uint64_t us = inl(RTC_ADDR) - boot_time; 
+  uptime->us = us;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
