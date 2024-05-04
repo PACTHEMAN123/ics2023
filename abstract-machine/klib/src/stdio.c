e84ee4e707c7b0249e0488d1ace68874af8d4c10
tracer-ics2023 <tracer@njuics.org>
2024-05-04 14:57:01 +0800

    >  compile NEMU 2023311422 pacman Linux matebook 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:57:01 up 11 days, 20:26,  3 users,  load average: 0.00, 0.00, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -7,46 +7,58 @@
 
 int printf(const char *fmt, ...) {
   va_list ap;
-  int d;
-  char *s;
-  char str[12];  
-  int count = 0;
-  
   va_start(ap, fmt);
-  while (*fmt) {
-    if(*fmt == '%') {
-      fmt ++;
-      if(*fmt == 'd') {
-	// demical number operation
-	d = va_arg(ap, int);
-	// int to string
-	int i = 0;
-	while (d != 0) {
-	  str[i++] = (d % 10) + '0';
-	  d /= 10;
+  int count = 0;
+  while(*fmt)
+  {
+    if(*fmt == '%')
+    {
+      char c;
+      int width = 0;
+      int pad_zero = 0;
+      int d;
+      int i;
+      int ifbreak = 1;
+      char *s;
+      char str[12];
+      while(ifbreak) {
+	c = *(++fmt);
+	switch(c) {
+	case '0':
+	  pad_zero = 1;
+	  i = 0;
+	  while((*(fmt + 1)>'0')&&(*(fmt + 1)<='9')){str[i++]=*(++fmt);}
+	  str[i] = '\0';
+	  width = atoi(str);
+	  break;
+	case 'd':
+	  d = va_arg(ap,int);
+	  i = 0;
+	  while(d!=0){str[i++] = (d%10)+'0';d/=10;}
+	  if(pad_zero){
+	    int num_zero = width - i;
+	    while(num_zero--){putch('0');count++;}
+	  }
+	  while(i>0){putch(str[--i]);count++;}
+	  ifbreak = 0;
+	  break;
+	case 's':
+	  s = va_arg(ap, char *);
+	  while(*s){putch(*s++);count++;}
+	  ifbreak = 0;
+	  break;
 	}
-	while (i > 0) { putch(str[--i]); count++; }
       } 
-      else if(*fmt == 's') {
-	// string operation
-	s = va_arg(ap, char *);
-	while(*s) {
-	  putch(*s++);
-	  count++;	
-	}
-      }
-      else {
-	fmt --; 
-      }
     }
-    else {
+    else
+    {
       putch(*fmt);
       count++;
-    }
+    } 
     fmt ++;
   }
   va_end(ap);
-  return count;
+  return count; 
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
