1bf081709d9bb78e154179fa80283cda978345ad
tracer-ics2023 <tracer@njuics.org>
2024-05-04 18:50:09 +0800

    >  compile NEMU 2023311422 pacman Linux matebook 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:50:09 up 12 days, 19 min,  4 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/device/serial.c
+++ nemu/src/device/serial.c
@@ -42,10 +42,10 @@ static void serial_io_handler(uint32_t offset, int len, bool is_write) {
 
 void init_serial() {
   serial_base = new_space(8);
-  Log("here");
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("serial", CONFIG_SERIAL_PORT, serial_base, 8, serial_io_handler);
 #else
+Log("here");
   add_mmio_map("serial", CONFIG_SERIAL_MMIO, serial_base, 8, serial_io_handler);
 #endif
 
