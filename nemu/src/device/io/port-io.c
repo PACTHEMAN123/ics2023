02565b32c8e6f2709971f56690315546cc4fc503
tracer-ics2023 <tracer@njuics.org>
2024-05-04 18:42:16 +0800

    >  compile NEMU 2023311422 pacman Linux matebook 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:42:16 up 12 days, 11 min,  4 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/device/io/port-io.c
+++ nemu/src/device/io/port-io.c
@@ -23,6 +23,7 @@ static int nr_map = 0;
 
 /* device interface */
 void add_pio_map(const char *name, ioaddr_t addr, void *space, uint32_t len, io_callback_t callback) {
+  Log("nr_map:%d",nr_map); 
   assert(nr_map < NR_MAP);
   assert(addr + len <= PORT_IO_SPACE_MAX);
   maps[nr_map] = (IOMap){ .name = name, .low = addr, .high = addr + len - 1,
