e7b0cd84bf76d0b203063eaa4d369ffb4add6a4c
tracer-ics2023 <tracer@njuics.org>
2024-05-04 12:52:12 +0800

    >  compile NEMU 2023311422 pacman Linux matebook 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  12:52:12 up 11 days, 18:21,  3 users,  load average: 0.04, 0.01, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -55,44 +55,57 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 
 int sprintf(char *out, const char *fmt, ...) {
   va_list ap;
-  int d;
-  char *s;
-  char str[12];  
-  
   va_start(ap, fmt);
-  while (*fmt) {
-    if(*fmt == '%') {
-      fmt ++;
-      if(*fmt == 'd') {
-	// demical number operation
-	d = va_arg(ap, int);
-	// int to string
-	int i = 0;
-	while (d != 0) {
-	  str[i++] = (d % 10) + '0';
-	  d /= 10;
+  int count = 0;
+  while(*fmt)
+  {
+    if(*fmt == '%')
+    {
+      char c;
+      int width = 0;
+      int pad_zero = 0;
+      int d;
+      int i;
+      int ifbreak = 1;
+      char *s;
+      char str[12];
+      while(ifbreak) {
+	c = *(++fmt);
+	switch(c) {
+	case '0':
+	  pad_zero = 1;
+	  i = 0;
+	  while((*(fmt + 1)>'0')&&(*(fmt + 1)<='9')){str[i++]=*(++fmt);}
+	  str[i] = '\0';
+	  width = atoi(str);
+	  break;
+	case 'd':
+	  d = va_arg(ap,int);
+	  i = 0;
+	  while(d!=0){str[i++] = (d%10)+'0';d/=10;}
+	  if(pad_zero){
+	    int num_zero = width - i;
+	    while(num_zero--)out[count++]='0';
+	  }
+	  while(i>0){out[count++]=str[--i];}
+	  ifbreak = 0;
+	  break;
+	case 's':
+	  s = va_arg(ap, char *);
+	  while(*s){out[count++]=*s++;}
+	  ifbreak = 0;
+	  break;
 	}
-	while (i > 0) { *out++ = str[--i]; }
       } 
-      else if(*fmt == 's') {
-	// string operation
-	s = va_arg(ap, char *);
-	while(*s) {
-	  *out++ = *s++ ;	
-	}
-      }
-      else {
-	fmt --; 
-      }
     }
-    else {
-      *out++ = *fmt ;
-    }
+    else
+    {
+      out[count++] = *fmt;
+    } 
     fmt ++;
   }
-  *out = '\0';
   va_end(ap);
-  return strlen(out); 
+  return count; 
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
