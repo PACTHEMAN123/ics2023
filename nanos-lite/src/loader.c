e2f2c817e039ec0cf00d39f354bf1d83f0ea2ea9
tracer-ics2023 <tracer@njuics.org>
2024-05-19 17:06:37 +0800

    >  compile NEMU 2023311422 pacman Linux matebook 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:06:37 up  6:18,  4 users,  load average: 0.10, 0.04, 0.01

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -22,7 +22,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
   Elf_Phdr phdr;
   ramdisk_read((void *)(&phdr), ehdr.e_phoff, sizeof(phdr));
- 
+  printf("here"); 
   for(int i = 0; i < phdr.p_filesz; i += bufsiz) {
     memset(buf, 0, bufsiz);
     size_t size = min(bufsiz, phdr.p_filesz - i);
