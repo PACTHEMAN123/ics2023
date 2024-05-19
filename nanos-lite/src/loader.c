793b388dc40c329d70d2bc9d417763731b923b23
tracer-ics2023 <tracer@njuics.org>
2024-05-19 21:10:51 +0800

    >  compile NEMU 2023311422 pacman Linux matebook 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:10:51 up  8:12,  4 users,  load average: 0.27, 0.06, 0.02

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -24,9 +24,8 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
     printf("filesz:%d memsz:%d vaddr:%p offset:%d", phdr.p_filesz, phdr.p_memsz, phdr.p_vaddr, phdr.p_offset);
     ramdisk_read((void *)(phdr.p_vaddr), phdr.p_offset, phdr.p_filesz); 
     memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
-    break;
   } 
-  return phdr.p_vaddr;
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
