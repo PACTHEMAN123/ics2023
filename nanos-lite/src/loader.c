b6962dc4977ccc8ef75d0e64c4ca05d5e41637be
tracer-ics2023 <tracer@njuics.org>
2024-05-19 17:18:14 +0800

    >  compile NEMU 2023311422 pacman Linux matebook 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:18:14 up  6:29,  4 users,  load average: 0.04, 0.02, 0.00

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,13 +9,9 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
-#define bufsiz 5120
-#define min(x,y) ((x < y) ? x : y)
 size_t ramdisk_read(void *, size_t, size_t);
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-
-  char buf[bufsiz];
 
   Elf_Ehdr ehdr;
   ramdisk_read((void *)(&ehdr), 0, sizeof(ehdr));
@@ -23,13 +19,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Phdr phdr;
   ramdisk_read((void *)(&phdr), ehdr.e_phoff, sizeof(phdr));
   printf("%d", phdr.p_filesz); 
-  for(int i = 0; i < phdr.p_filesz; i += bufsiz) {
-    memset(buf, 0, bufsiz);
-    size_t size = min((min(bufsiz, phdr.p_filesz - i)), phdr.p_filesz);
-    printf(" %d", size);
-    ramdisk_read(buf, phdr.p_offset, size);
-    strncpy((char *)(phdr.p_vaddr + i), buf, size);
-  }
+  ramdisk_read((void *)(phdr.p_vaddr), phdr.p_offset, phdr.p_filesz); 
   
   memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz); 
    
