1515bad8d2bcc57bd8fbfdfbe7d46e3a9f8df359
tracer-ics2023 <tracer@njuics.org>
2024-05-21 13:55:42 +0800

    >  compile NEMU 2023311422 pacman Linux matebook 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:55:42 up 22:05,  4 users,  load average: 0.00, 0.00, 0.00

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,9 +3,10 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  a[1] = c->GPR2;
   switch (a[0]) {
-    //case 1: yield(); c->GPRx = 0; break;
+    case 0: halt(a[1]);
+    case 1: yield(); c->GPRx = 0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
