e6c21d6e4bf788726ec1473e5efa4d89d94a89f7
tracer-ics2023 <tracer@njuics.org>
2024-05-24 00:06:55 +0800

    >  compile NEMU 2023311422 pacman Linux matebook 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:06:55 up 2 days, 18:04,  4 users,  load average: 0.15, 0.03, 0.01

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -61,7 +61,7 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-  return _syscall_((intptr_t)path, (intptr_t)flags, (intptr_t)mode);
+  return _syscall_((intptr_t)SYS_open, (intptr_t)path, (intptr_t)flags, (intptr_t)mode);
 }
 
 int _write(int fd, void *buf, size_t count) {
@@ -90,15 +90,15 @@ void *_sbrk(intptr_t increment) {
 }
 
 int _read(int fd, void *buf, size_t count) {
-  return _syscall_((intptr_t)SYS_read, (intptr_t)buf, (intptr_t)count);
+  return _syscall_((intptr_t)SYS_read, (intptr_t)buf, (intptr_t)count, 0);
 }
 
 int _close(int fd) {
-  return _syscall_((intptr_t)SYS_close, (intptr_t)fd);
+  return _syscall_((intptr_t)SYS_close, (intptr_t)fd, 0, 0);
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  return _syscall((intptr_t)SYS_lseek, (intptr_t)offset, (intptr_t)whence); 
+  return _syscall_((intptr_t)SYS_lseek, (intptr_t)offset, (intptr_t)whence, 0); 
 }
 
 int _gettimeofday(struct timeval *tv, struct timezone *tz) {
