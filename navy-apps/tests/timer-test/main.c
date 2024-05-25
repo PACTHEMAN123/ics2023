71a6d49a2e3cb2a388cdb27fb706c743d1426b45
tracer-ics2023 <tracer@njuics.org>
2024-05-26 00:29:57 +0800

    >  compile NEMU 2023311422 pacman Linux matebook 5.15.146.1-microsoft-standard-WSL2 #1 SMP Thu Jan 11 04:09:03 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:29:57 up 3 days, 13:49,  3 users,  load average: 0.07, 0.03, 0.00

--- navy-apps/tests/timer-test/main.c
+++ navy-apps/tests/timer-test/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <assert.h>
 #include <NDL.h>
+#include <unistd.h>
 
 int main() {
   int sec = 1;
   uint32_t usec;
+  NDL_Init(0);
   while(1) {
     usec = NDL_GetTicks();
     if((usec / 1000000) == sec) {
